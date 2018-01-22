2ade91812fc15f09d4b66df4e936ab48d28b4a86
DRakesh <DRakesh@LIN66003101>
2018-01-22 19:06:29 +0530

    Add project files.

---
+++ SpecFlow.Selenium/Features/Aviva.feature
@@ -0,0 +1,17 @@
+﻿Feature: Demo
+		As a User,
+		I Search for Aviva and verify the page
+
+@Positive
+Scenario: Google Search - Aviva
+	Given I am on "http://www.google.com"
+	When I search for "Aviva"
+	And select "Aviva" in the search results
+	Then I am presented with the "https://online.avivaindia.com/econnect/login.aspx" homepage
+
+@Negetive
+Scenario: Google Search - Aviva Login
+	Given I am on "http://www.google.com"
+	When I search for "Aviva"
+	And select "Aviva" in the search results
+	Then I am presented with the "https://online.avivaindia.com/" homepage
---
+++ SpecFlow.Selenium/Features/Support/Hooks.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BoDi;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using RelevantCodes.ExtentReports;
+using NUnit.Framework;
+
+namespace SpecFlow.Selenium.Features.Support
+{
+	[Binding]
+	public class Hooks
+	{
+      
+
+		private readonly IObjectContainer objectContainer;
+		private IWebDriver driver;
+		static DesiredCapabilities desiredCapabilities;
+
+		public Hooks(IObjectContainer objectContainer)
+		{
+			this.objectContainer = objectContainer;
+		}
+
+        
+		[BeforeTestRun]
+		public static void BeforeTestRun()
+		{
+		    // Switch to reading caps form Devices.json
+			Dictionary<string, object> caps = new Dictionary<string, object>();
+			caps.Add("browserName", "chrome");
+
+			desiredCapabilities = new DesiredCapabilities(caps);
+		}
+
+		[BeforeScenario]
+		public void BeforeScenario()
+		{
+            //To call Extent Report
+           
+            
+            driver = new ChromeDriver();
+
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			objectContainer.RegisterInstanceAs(driver);
+		}
+
+
+		[AfterScenario]
+		public void AfterScenario()
+		{
+			if (driver != null)
+			{
+                
+				driver.Dispose();
+			}
+		}
+
+
+	    [AfterTestRun]
+	    public static void AfterTestRun()
+	    {
+	        // code that runs after the test run
+	    }
+	}
+}
